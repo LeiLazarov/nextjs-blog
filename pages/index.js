07246db03f27202d8decd94bdef3471134b75d9d
leilazarov <stoneiiilei@gmail.com>
2021-09-22 23:57:13 +1000

    test pr

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <p>[Test Pull Request]</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
